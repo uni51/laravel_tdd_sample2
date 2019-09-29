d0d50e11de4a653f6f625556e69a77adb6233138
uni51 <uni51@users.noreply.github.com>
2019-09-30 06:27:43 +0900

    11-2-3 テストメソッドに何をどのように書くか

--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -13,5 +13,9 @@ class ReportTest extends TestCase
      */
     public function api_customersにGETメソッドでアクセスできる()
     {
+        // 実行部分を記述
+        $response = $this->get('api/customers');
+        // 先に検証部分を記述
+        $response->assertStatus(200);
     }
 }
