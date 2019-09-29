1ece48aa75aaf6bf6515065325efd798d21a3a3e
uni51 <uni51@users.noreply.github.com>
2019-09-30 06:23:32 +0900

    11-2-2 最初のテスト

---
+++ tests/Feature/ReportTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ReportTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function api_customersにGETメソッドでアクセスできる()
+    {
+    }
+}
